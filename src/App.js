4e67aa1052b8f2ee09d46f5fcef9557170ab0fb6
Vladislav <anitvlad288@gmail.com>
2023-10-27 19:25:16 +0300

    normal useRef

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { useLocalStorage } from './hooks/use-localStorage.hook.js';
 import './App.css';
 
@@ -7,69 +7,26 @@ function App() {
 	const [userLastname, setUserLastname] = useState('');
 	const [userAge, setUserAge] = useState('');
 
-	const [userNameClass, setUserNameClass] = useState('');
-	const [userLastnameClass, setUserLastnameClass] = useState('');
-	const [userAgeClass, setUserAgeClass] = useState('');
+	const input1Ref = useRef(null);
+	const input2Ref = useRef(null);
+	const input3Ref = useRef(null);
 
-	const [valid, setValid] = useState({
-		name: true,
-		lastname: true,
-		age: true
-	});
-
-	const validAccet = () => {
-		let newValid = {
-			name: false,
-			lastname: false,
-			age: false
-		};
-
-		if (userName.length >= 2 && userName.length <= 10) {
-			newValid.name = true;
-		}
-		if (userLastname.length >= 2 && userLastname.length <= 10) {
-			newValid.lastname = true;
-		}
-		if (userAge.length >= 1 && userAge.length <= 3) {
-			newValid.age = true;
-		}
-
-		setValid(newValid);
-	};
-
-	const isValib = () => {
-		if (valid.name === true && valid.lastname === true && valid.age === true) {
-			setUserNameClass('valid');
-			setUserLastnameClass('valid');
-			setUserAgeClass('valid');
-		} else {
-			if (valid.name === false) {
-				setUserNameClass('notValid');
-			}
-			if (valid.lastname === false) {
-				setUserLastnameClass('notValid');
-			}
-			if (valid.age === false) {
-				setUserAgeClass('notValid');
-			}
-		}
-	};
+	// function handleClick() {
+    //     inputRef.current.focus();
+    // }
 
 	const formRep = (event) => {
 		event.preventDefault();
 
-		if (valid.name === true && valid.lastname === true && valid.age === true) {
-			addData({
-				name: userName,
-				lastname: userLastname,
-				age: userAge
-			});
+		addData({
+			name: userName,
+			lastname: userLastname,
+			age: userAge
+		});
 
-			setUserName('');
-			setUserLastname('');
-			setUserAge('');
-		}
-		isValib();
+		setUserName('');
+		setUserLastname('');
+		setUserAge('');
 	};
 
 	const [elements, setElements] = useLocalStorage('data');
@@ -84,34 +41,31 @@ function App() {
 			<div className="Main">
 				<form onSubmit={formRep} className="Form">
 					<input
-						className={userNameClass}
 						onChange={(event) => {
-							validAccet();
 							return setUserName(event.target.value);
 						}}
 						value={userName}
 						name="name"
 						placeholder="Имя"
+						ref={input1Ref}
 					/>
 					<input
-						className={userLastnameClass}
 						onChange={(event) => {
-							validAccet();
 							return setUserLastname(event.target.value);
 						}}
 						value={userLastname}
 						name="lastname"
 						placeholder="Фамилия"
+						ref={input2Ref}
 					/>
 					<input
-						className={userAgeClass}
 						onChange={(event) => {
-							validAccet();
 							return setUserAge(event.target.value);
 						}}
 						value={userAge}
 						name="age"
 						placeholder="Возраст"
+						ref={input3Ref}
 					/>
 					<button type="submit">Подобрать котика</button>
 				</form>
